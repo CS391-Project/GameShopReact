639118d9677088c5ba8c8ca704824a1a7c460e78
OguzErtanAyanlar <oguzertanayanlar@gmail.com>
2021-01-14 15:10:51 +0300

    Shop fixes

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,32 +1,33 @@
 import React from "react"
 import {Button} from "reactstrap";
-import MoreInfoPage from "./MoreInfoPage";
 import data from "../Data/data.json";
-
-const productData = data.productList;
-
-function buttonAction(props){
-    localStorage.setItem("id",props.id)
-    {<MoreInfoPage/>}
-    window.location.href="/moreInfoPage"
-}
+import { useHistory } from "react-router-dom";
 
 const Shop = (props) => {
+    const history = useHistory();
+    const goToPage = (pageName) => history.push(pageName);
+    const productData = data.productList;
+
+    const buttonAction = (props) => {
+        localStorage.setItem("id", props.id)
+        goToPage('moreInfoPage')
+    }
+
     return (
         <div id="shop">
             <h2>Shop</h2>
-            { productData.filter((product) =>!(props.activeFilters.includes(product.category))).map((product, i) => (
-                <form key={i} id={product.name}>
+            {productData.filter((product) => !(props.activeFilters.includes(product.category))).map((product, i) => (
+                <div>
                     <p>{product.name}</p>
                     <p>Category: {product.category}</p>
                     <p>Price: {product.price}</p>
-                    <img src={process.env.PUBLIC_URL + "/images/"+product.link} alt={product.name}/>
+                    <img src={process.env.PUBLIC_URL + "/images/" + product.link} alt={product.name}/>
 
-                    <Button variant="btn btn-success" onClick={()=>buttonAction(product)}>More Info</Button>
+                    <Button variant="btn btn-success" onClick={() => buttonAction(product)}>More Info</Button>
 
                     <br></br>
                     <br></br>
-                </form>
+                </div>
             ))}
         </div>
     );
